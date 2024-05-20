fcc282b8060124de2c2370080e7bec8c31bee1d8
Adityasingh77777 <singhadityakumar2022@gmail.com>
2024-05-21 01:35:16 +0530

    understanding main jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,7 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+        <div>
+            <h1>
+                Custom App!
+            </h1>
+        </div>
+    )
+}
+
+// const reactElement ={
+//     type:'a',// different tags in the html
+//     props:{
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google'
+// }
+
+// now we are going to make reactElement 
+// according to react not for custom
+
+const reactElement=React.createElement(
+    'a',
+    {href:'https://google.com',target:'_blank'},
+    'click me to visit google'
+)
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).
 render(
-    <App />
+    // <MyApp />  // this can be written as MyApp() 
+    // anotherElement  
+    // reactElement
+
+    <App/>
 )
+
+
+// reactElement ka syntax sahi nahi h kyuki iske liye humlog ne custom render 
+// banae the
