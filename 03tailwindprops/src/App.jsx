9db11c48ac05f92261d6094fc382358d381c7f20
Adityasingh77777 <singhadityakumar2022@gmail.com>
2024-05-25 18:30:38 +0530

    message

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -5,13 +5,17 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-
+    let myObj={
+      username:"Aditya",
+      age:20
+    }
   return (
     <>
       <h1 className='bg-green-400 
       text-black 
       p-4 
       rounded-xl mb-4' >Tailwind Test</h1>
+      <Card channel="chai aur code" someObj={myObj}/>
       <Card/>
     </>
   )
