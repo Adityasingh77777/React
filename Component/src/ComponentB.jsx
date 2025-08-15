15df4366abe7f9d6a8b834a0d8b87966c98d28ad
Adityasingh77777 <aditya.2201139ec@iiitbh.ac.in>
2025-08-15 15:56:03 +0530

    Component learned

---
+++ Component/src/ComponentB.jsx
@@ -0,0 +1,13 @@
+import React, {useContext} from 'react'
+import ComponentC from './ComponentC.jsx';
+
+function ComponentB(props){
+    return (
+        <div className="box">
+        <h1>ComponentB</h1>
+        <ComponentC user={props.user}/>
+        </div>
+    );
+}
+
+export default ComponentB
